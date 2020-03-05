422c4da80976b3aff18f85b339673bde0a4486ab
NadineMarques <nadymarkes@gmail.com>
2020-03-04 22:08:56 -0300

    Atualização Telas Admin

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,16 +2,26 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-header">Bem Vindo, Administrador</div>
+
+                <div class="d-flex card-body justify-content-center">
+                    <a href="{{route('home')}} " class="btn-btn success admin-a"><i class="fas fa-user-cog fa-5x"></i></a>
+
+                    <!-- <img src="{{ asset('images/admin-user.png') }}" class="rounded-circle"> -->
+                </div>
+            </div>
+        </div>
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                        Administrador Logado
-                        <a href="{{route('admin.index')}} " class="btn-btn success">Mostrar Usuários</a>
-                        <a href="{{route('challenges.index')}} " class="btn-btn success">Mostrar Desafios</a>
-                        <a href="{{route('challenges.create')}} " class="btn-btn success">Criar Desafios</a>
+                <div class="card-body admin-card-body">
+                    <a href="{{route('admin.index')}} " class="btn-btn success admin-a"><i class="fas fa-users fa-3x"></i>Mostrar Usuários</a>
+                    <a href="{{route('challenges.index')}} " class="btn-btn success admin-a"><i class="fas fa-tasks fa-3x"></i>Mostrar Desafios</a>
+                    <a href="{{route('challenges.create')}} " class="btn-btn success admin-a"><i class="fas fa-plus-square fa-3x"></i>Criar Desafios</a>
                 </div>
             </div>
         </div>
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,40 +1,46 @@
 @extends('layouts.app')
 @section('content')
-<table class="table">
-    <thead>
-    <tr>
-        <th scope="col">#</th>
-        <th scope="col">Nome</th>
-        <th scope="col">Email</th>
-        <th scope="col">Admin?</th>
-        <th scope="col">Company?</th>
-        <th scope="col">Ação</th>
-    </tr>
-    </thead>
-    <tbody>
-    @foreach($users as $user)
-        <tr>
-            <th scope="row">{{$user->id}}</th>
-            <td>{{$user->name}}</td>
-            <td>{{$user->email}}</td>
-            <td>{{$user->admin == 1 ? "Sim" : "Não"}}</td>
-            <td>{{$user->company == 1 ? "Sim" : "Não"}}</td>
-            <td>
-                <a class="btn btn-success" href="{{route('admin.edit',$user->id)}}">Editar</a>
-                @if($user->id != Auth::id())
-                    <form method="POST" action="{{route('admin.destroy', $user->id)}}">
-                        @csrf
-                        {!! method_field('delete') !!}
-                        <input type="submit" class="btn btn-danger botaoColor apagar" value="Apagar Usuário">
-                    </form>
-                    @endif
-            </td>
-        </tr>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <table class="table">
+                <thead>
+                    <tr class="admin-tr">
+                        <th scope="col">#</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Admin?</th>
+                        <th scope="col">Company?</th>
+                        <th scope="col">Ação</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($users as $user)
+                    <tr>
+                        <th scope="row">{{$user->id}}</th>
+                        <td>{{$user->name}}</td>
+                        <td>{{$user->email}}</td>
+                        <td>{{$user->admin == 1 ? "Sim" : "Não"}}</td>
+                        <td>{{$user->company == 1 ? "Sim" : "Não"}}</td>
+                        <td>
+                            <a class="btn btn-success admin-edit-td" href="{{route('admin.edit',$user->id)}}">Editar</a>
+                            @if($user->id != Auth::id())
+                            <form method="POST" action="{{route('admin.destroy', $user->id)}}">
+                                @csrf
+                                {!! method_field('delete') !!}
+                                <input type="submit" class="btn btn-danger botaoColor apagar admin-remove-td" value="Apagar Usuário">
+                            </form>
+                            @endif
+                        </td>
+                    </tr>
 
 
-    @endforeach
+                    @endforeach
 
-    </tbody>
-</table>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 
-    @endsection
+@endsection
